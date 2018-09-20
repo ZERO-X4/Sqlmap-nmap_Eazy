c3f7406dfa363495e53263b5da853debb2a4847b
clash <35383814+yazeed33@users.noreply.github.com>
2018-09-20 14:49:33 +0300

    Update Use_Sql_Map_Eazy.py

--- Use_Sql_Map_Eazy.py
+++ Use_Sql_Map_Eazy.py
@@ -5,4 +5,15 @@ print("+           My Email: Bilalyazeed4@gmail.com               +")
 print("+                                                          +")
 print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
-import smtplib 
+import os
+
+url = input("Enter Url: ")
+print("+++++++++++++++++++++++++++++++++++++++++++++")
+sqlmap1 = os.system('sqlmap --url {} --dbs {} --random-agent'.format(url))
+print("+++++++++++++++++++++++++++++++++++++++++++++")
+dbs = input("Enter DATABASE Name: ")
+sqlmap1 = os.system('sqlmap --url {} -D {} --tables {} --random-agent'.format(url,dbs))
+tab = input("Enter TABLE Name: ")
+sqlmap1 = os.system('sqlmap --url {} -D {} -T {} --random-agent'.format(url,dbs,tab))
+col = input("Enter COLUMN name: ")
+sqlmap1 = os.system('sqlmap --url {} -D {} -T {} -C {} --random-agent'.format(url,dbs,tab,col))
