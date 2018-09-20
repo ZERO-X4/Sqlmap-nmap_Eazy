2f49e2e2e3c7de0f990e8a4d3c94389a25f17019
clash <35383814+yazeed33@users.noreply.github.com>
2018-09-20 13:39:01 +0300

    Create OUTLOOK_BRUTE_FORCE.py

---
+++ OUTLOOK_BRUTE_FORCE.py
@@ -0,0 +1,8 @@
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+print("+                                                          +")
+print("+               [!] powered by yazeed [!]                  +")
+print("+           My Email: Bilalyazeed4@gmail.com               +")
+print("+                                                          +")
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+import smtplib
