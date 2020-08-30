bc6e4280c247809b124818c6c5621fcc4f3ddbfd
Yazeed Bilal <35383814+ZERO-X4@users.noreply.github.com>
2020-08-30 16:22:03 +0000

    Update Use_Nmap_Eazy.py

--- Use_Nmap_Eazy.py
+++ Use_Nmap_Eazy.py
@@ -1,6 +1,7 @@
 print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 print("+                                                          +")
-print("+               [!] powered by yazeed [!]                  +")
+print("+           [!] powered by  Yazeed Bilal () zerox4 [!]     +")
+print("+                                                          +")
 print("+           My Email: Bilalyazeed4@gmail.com               +")
 print("+                                                          +")
 print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
